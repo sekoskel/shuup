5383d48dd6bce90e3857da7344c517e71fb2488d
Janne Tammi <janne@shuup.com>
2017-03-08 15:27:56 +0200

    Company registration
    This is still work in progress but this commit will enable
    companies to register in Shuup

--- shuup_tests/admin/test_settings.py
+++ shuup_tests/admin/test_settings.py
@@ -17,7 +17,7 @@ from shuup.admin.modules.settings.views import SystemSettingsView
 from shuup.testing.utils import apply_request_middleware
 
 
-def set_reference_method(rf, admin_user, reference_method, shop=None):
+def assert_config_value(rf, admin_user, form_id, key, value, expected_value, shop=None):
     if not shop:
         shop = get_default_shop()
     request = apply_request_middleware(rf.get("/"), user=admin_user)
@@ -25,15 +25,22 @@ def set_reference_method(rf, admin_user, reference_method, shop=None):
     response = view_func(request)
     assert response.status_code == 200
 
-    data = {
-        "order_reference_number_method": reference_method.value
-    }
+    form_field = "%s-%s" % (form_id, key)
+    data = {form_field: value}
     request = apply_request_middleware(rf.post("/", data=data))
-    view_func(request)
-    assert configuration.get(None, consts.ORDER_REFERENCE_NUMBER_METHOD_FIELD) == reference_method.value
+    response = view_func(request)
+    assert response.status_code == 302
+    if expected_value == "unset":
+        expected_value = value
+    assert configuration.get(None, key) == expected_value
     return shop
 
 
+@pytest.mark.parametrize("form_id, field, value, expected_value, shop", [
+    ("order_settings", consts.ORDER_REFERENCE_NUMBER_METHOD_FIELD, OrderReferenceNumberMethod.UNIQUE.value, "unset", None),
+    ("registration_settings", consts.SETTING_ALLOW_COMPANY_REGISTRATION, True, "unset", None),
+    ("registration_settings", consts.SETTING_ALLOW_COMPANY_REGISTRATION, "", False, None)
+])
 @pytest.mark.django_db
-def test_system_settings(rf, admin_user):
-    set_reference_method(rf, admin_user, OrderReferenceNumberMethod.UNIQUE)
+def test_system_settings(rf, admin_user, form_id, field, value, expected_value, shop):
+    assert_config_value(rf, admin_user, form_id, field, value, expected_value, shop)
